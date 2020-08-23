56eff542c80de90464e4b9ad7bf19363aed7861c
Jingyun Bian <jingbian@amazon.com>
2020-08-22 20:28:13 -0400

    Updates

---
+++ precache-manifest.54c0cdd02bd327a42ec671b4fce9595e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28c772641a3904b8d62de1084b0dd82",
+    "url": "/color-boxes/index.html"
+  },
+  {
+    "revision": "daf6a19bf9a1a4916b3d",
+    "url": "/color-boxes/static/css/main.c169b865.chunk.css"
+  },
+  {
+    "revision": "f3487d760175e953e0a6",
+    "url": "/color-boxes/static/js/2.50f53058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-boxes/static/js/2.50f53058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf6a19bf9a1a4916b3d",
+    "url": "/color-boxes/static/js/main.7cc10023.chunk.js"
+  },
+  {
+    "revision": "485e4ed8fa72a7b3d07f",
+    "url": "/color-boxes/static/js/runtime-main.db813850.js"
+  }
+]);
